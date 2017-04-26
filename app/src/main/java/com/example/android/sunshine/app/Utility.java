3c20769e4f31f188c27aa0993269ef05e6635b86
Anh Hoang <h.anh.viet91@gmail.com>
2017-04-26 22:14:49 +0300

    updated icons personalization

--- app/src/main/java/com/example/android/sunshine/app/Utility.java
+++ app/src/main/java/com/example/android/sunshine/app/Utility.java
@@ -254,29 +254,34 @@ public class Utility {
         return -1;
     }
 
-    public static String getArtUrlForWeatherCondition(Context context, int weatherId){
+    public static String getArtUrlForWeatherCondition(Context context, int weatherId) {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+        String formatArtUrl = preferences.getString(
+                context.getString(R.string.pref_art_pack_key),
+                context.getString(R.string.pref_art_pack_sunshine));
+
         if (weatherId >= 200 && weatherId <= 232) {
-            return context.getString(R.string.format_art_url, "storm");
+            return String.format(formatArtUrl, "storm");
         } else if (weatherId >= 300 && weatherId <= 321) {
-            return context.getString(R.string.format_art_url, "light_rain");
+            return String.format(formatArtUrl, "light_rain");
         } else if (weatherId >= 500 && weatherId <= 504) {
-            return context.getString(R.string.format_art_url, "rain");
+            return String.format(formatArtUrl, "rain");
         } else if (weatherId == 511) {
-            return context.getString(R.string.format_art_url, "snow");
+            return String.format(formatArtUrl, "snow");
         } else if (weatherId >= 520 && weatherId <= 531) {
-            return context.getString(R.string.format_art_url, "rain");
+            return String.format(formatArtUrl, "rain");
         } else if (weatherId >= 600 && weatherId <= 622) {
-            return context.getString(R.string.format_art_url, "snow");
+            return String.format(formatArtUrl, "snow");
         } else if (weatherId >= 701 && weatherId <= 761) {
-            return context.getString(R.string.format_art_url, "fog");
+            return String.format(formatArtUrl, "fog");
         } else if (weatherId == 761 || weatherId == 781) {
-            return context.getString(R.string.format_art_url, "storm");
+            return String.format(formatArtUrl, "storm");
         } else if (weatherId == 800) {
-            return context.getString(R.string.format_art_url, "clear");
+            return String.format(formatArtUrl, "clear");
         } else if (weatherId == 801) {
-            return context.getString(R.string.format_art_url, "light_clouds");
+            return String.format(formatArtUrl, "light_clouds");
         } else if (weatherId >= 802 && weatherId <= 804) {
-            return context.getString(R.string.format_art_url, "clouds");
+            return String.format(formatArtUrl, "clouds");
         }
         return null;
     }
@@ -296,7 +301,7 @@ public class Utility {
 
     }
 
-    public static void resetLocationStatus(Context context){
+    public static void resetLocationStatus(Context context) {
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
         SharedPreferences.Editor editor = sharedPreferences.edit();
         editor.putInt(context.getString(R.string.pref_location_status_key), SunshineSyncAdapter.LOCATION_STATUS_UNKNOWN);
